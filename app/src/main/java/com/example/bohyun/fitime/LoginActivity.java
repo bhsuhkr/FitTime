d6970696a09f2b3e80bc76b34534e917014853a8
Bohyun Suh <bhsuhkr@naver.com>
2017-10-17 12:29:34 -0500

    add start button

--- app/src/main/java/com/example/bohyun/fitime/LoginActivity.java
+++ app/src/main/java/com/example/bohyun/fitime/LoginActivity.java
@@ -1,13 +1,13 @@
 package com.example.bohyun.fitime;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
