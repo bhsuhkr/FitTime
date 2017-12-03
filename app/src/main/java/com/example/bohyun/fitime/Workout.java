8a4399c5bf92c1ab1af9b295ac62a0cb92ee0d16
Justin <justinmwright96@gmail.com>
2017-12-03 16:01:58 -0600

    workouts activity

---
+++ app/src/main/java/com/example/bohyun/fitime/Workout.java
@@ -0,0 +1,28 @@
+package com.example.bohyun.fitime;
+/**
+ * Created by zschr on 11/28/2017.
+ */
+
+public class Workout {
+
+    String name, description, exercises;
+
+    public Workout(String name, String description, String exercises) {
+        this.name = name;
+        this.description = description;
+        this.exercises = exercises;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getExercises() {
+            return exercises;
+    }
+}
