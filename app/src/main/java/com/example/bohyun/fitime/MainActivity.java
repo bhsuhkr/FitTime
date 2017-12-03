313ad8b8057c80d1a4a684ef654e11c64ecb6251
Green <green@localhost.localdomain>
2017-12-02 21:36:37 -0600

    loginFix v3

--- app/src/main/java/com/example/bohyun/fitime/MainActivity.java
+++ app/src/main/java/com/example/bohyun/fitime/MainActivity.java
@@ -89,4 +89,14 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
+
+    @Override
+    public void onBackPressed() {
+        // your code.
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
 }
